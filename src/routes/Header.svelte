a657b4160222f69ac15788b1aeba6cc57bf7142b
hybridherbst <felix.herbst@gmail.com>
2023-07-29 18:26:29 +0200

    ptr, styling

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -124,9 +124,6 @@
 		align-items: center;
 		padding: 0 0.5rem;
 		color: var(--color-text);
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
 		text-decoration: none;
 		transition: color 0.2s linear;
 	}
